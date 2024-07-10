569450d194ac84c9e4bcf7ca6f9bc9d577652ddb
Alya Kavalenka <kovalenkoalinam@gmail.com>
2024-07-10 13:27:57 +0300

    feat: open/close modal by js

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
-const str = 'jfjf';
-const arr: string[] = [];
+const modal = document.getElementById('modal');
+const btnTry = document.getElementById('btn-try');
+const btnClose = document.getElementById('btn-close');
 
-arr.push('fjf');
+function setModalState(modalState: 'hidden' | 'visible') {
+  if (modal) {
+    modal.style.visibility = modalState;
+  }
+}
 
-console.log('in index');
+setModalState('hidden');
+
+if (btnTry) {
+  btnTry.onclick = () => setModalState('visible');
+}
+
+if (modal) {
+  modal.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if (target && target instanceof HTMLElement) {
+      if (target.classList.contains('modal')) {
+        setModalState('hidden');
+      }
+    }
+  });
+}
+
+if (btnClose) {
+  btnClose.onclick = () => setModalState('hidden');
+}
